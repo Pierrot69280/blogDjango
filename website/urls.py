96973bdb4068145766b27bf7148081d3f5a73fb1
Pierre <pierre.nogaro@gmail.com>
2024-10-24 21:09:47 +0200

    delete article

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.article_index, name='article_index'),
     path('article/<int:article_id>/', views.article_show, name='article_show'),
+    path('article/<int:article_id>/delete/', views.article_delete, name='article_delete'),
+
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article
 
 def article_index(request):
@@ -9,3 +9,8 @@ def article_show(request, article_id):
     article = Article.objects.get(id=article_id)
     return render(request, 'website/articles/show.html', {'article': article})
 
+
+def article_delete(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    article.delete()
+    return redirect('article_index')
