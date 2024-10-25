ea29f77a0ee2ba44691e9b8941318e2acb778bd4
Pierre <pierre.nogaro@gmail.com>
2024-10-25 14:18:17 +0200

    login/register & logout

--- website/urls.py
+++ website/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('article/<int:article_id>/delete/', views.article_delete, name='article_delete'),
     path('article/create/', views.article_create, name='article_create'),
     path('article/edit/<int:article_id>/', views.article_edit, name='article_edit'),
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
 ]
 
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import ArticleForm
+from .forms import ArticleForm, SignupForm, LoginForm
 from .models import Article
 
 def article_index(request):
@@ -38,3 +39,34 @@ def article_edit(request, article_id):
     else:
         articleForm = ArticleForm(instance=article)
     return render(request, "website/articles/edit.html", {"articleForm": articleForm})
+
+
+def user_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'website/registration/signup.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('article_index')
+    else:
+        form = LoginForm()
+    return render(request, 'website/registration/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
