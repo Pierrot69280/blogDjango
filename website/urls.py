60217565e1721bb25ed0d887d2cd23d5d0714c90
Pierre <pierre.nogaro@gmail.com>
2024-10-25 13:24:49 +0200

    create & update article

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.article_index, name='article_index'),
     path('article/<int:article_id>/', views.article_show, name='article_show'),
     path('article/<int:article_id>/delete/', views.article_delete, name='article_delete'),
-
+    path('article/create/', views.article_create, name='article_create'),
+    path('article/edit/<int:article_id>/', views.article_edit, name='article_edit'),
 ]
+
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ArticleForm
 from .models import Article
 
 def article_index(request):
@@ -14,3 +15,27 @@ def article_delete(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     article.delete()
     return redirect('article_index')
+
+def article_create(request):
+    if request.method == "POST":
+        articleForm = ArticleForm(request.POST)
+        if articleForm.is_valid():
+            articleForm.save()
+            return redirect('article_index')
+    else:
+        articleForm = ArticleForm()
+    return render(request, "website/articles/create.html", {"articleForm": articleForm})
+
+
+def article_edit(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+
+    if request.method == "POST":
+        articleForm = ArticleForm(request.POST, instance=article)
+        if articleForm.is_valid():
+            articleForm.save()
+            return redirect('article_index')
+    else:
+        articleForm = ArticleForm(instance=article)
+
+    return render(request, "website/articles/edit.html", {"articleForm": articleForm})
